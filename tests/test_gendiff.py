bebdf1f4c125d68c4fe1e6c8aa8d61b34cf4934a
Chawku <chawku.work@gmail.com>
2024-10-08 18:48:36 +0300

    update tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,39 +1,20 @@
-import json
 import pytest
 from gendiff.generate_diff import generate_diff
 
 
-def create_temp_json_file(content):
-    with open("temp_file.json", "w") as f:
-        json.dump(content, f)
-
 def test_generate_diff():
-    data1 = {
-        "host": "hexlet.io",
-        "timeout": 50,
-        "proxy": "123.234.53.22",
-        "follow": False
-    }
-
-    data2 = {
-        "timeout": 20,
-        "verbose": True,
-        "host": "hexlet.io"
-    }
-
-    create_temp_json_file(data1)
-    create_temp_json_file(data2)
-
-    result = generate_diff("temp_file.json", "temp_file.json")
+    file1_path = "tests/fixtures/file1.json"
+    file2_path = "tests/fixtures/file2.json"
     expected_output = (
         "{\n"
-        "  - follow: false\n"
+        "  - follow: False\n"
         "  - proxy: 123.234.53.22\n"
         "  - timeout: 50\n"
         "  + timeout: 20\n"
-        "  + verbose: true\n"
+        "  + verbose: True\n"
         "}"
     )
 
+    result = generate_diff(file1_path, file2_path)
+
     assert result.strip() == expected_output.strip()
-    
