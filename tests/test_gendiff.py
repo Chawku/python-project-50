53802690d9f5b92a9fd26105dc75d98df12c5870
Chawku <chawku.work@gmail.com>
2024-11-29 19:27:08 +0300

    update tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,13 +1,10 @@
 import os
 import pytest
-import json
-from gendiff.generate_diff import make_diff, build_diff, generate_diff
-
+from gendiff.generate_diff import generate_diff
 
 @pytest.fixture
 def fixture_path():
     return os.path.join("tests", "fixtures")
-
 
 @pytest.fixture
 def load_file_content(fixture_path):
@@ -17,26 +14,22 @@ def load_file_content(fixture_path):
             return f.read()
     return _load_file
 
-
-def test_make_diff(load_file_content):
-    dict1 = json.loads(load_file_content("file1.json"))
-    dict2 = json.loads(load_file_content("file2.json"))
-    expected = json.loads(load_file_content("expected_make_diff.txt"))
-
-    assert make_diff(dict1, dict2) == expected
-
-
-def test_build_diff(load_file_content):
-    dict1 = json.loads(load_file_content("file1.json"))
-    dict2 = json.loads(load_file_content("file2.json"))
-    expected = json.loads(load_file_content("expected_build_diff.txt"))
-
-    assert build_diff(dict1, dict2) == expected
+@pytest.mark.parametrize(
+    "file1, file2, expected_file, format_of_output",
+    [
+        ("file1.json", "file2.json", "expected_output.txt", "json"),
+        ("file1.yaml", "file2.yaml", "expected_stylish_output.txt", "stylish"),
+        ("file1_tree.json", "file2_tree.yaml", "expected_tree_stylish_output.txt", "stylish"),
+        ("file1_tree.json", "file2_tree.yaml", "expected_plain_output.txt", "plain"),
+    ]
+)
 
 
-def test_generate_diff(load_file_content):
-    file1 = os.path.join("tests", "fixtures", "file1.json")
-    file2 = os.path.join("tests", "fixtures", "file2.json")
-    expected_stylish_output = load_file_content("expected_stylish_output.txt")
+def test_generate_diff(file1, file2, expected_file, format_of_output, load_file_content, fixture_path):
+    file1_path = os.path.join(fixture_path, file1)
+    file2_path = os.path.join(fixture_path, file2)
+    expected_output = load_file_content(expected_file)
 
-    assert generate_diff(file1, file2, format_of_output='stylish') == expected_stylish_output
+    result = generate_diff(file1_path, file2_path, format_of_output=format_of_output)
+
+    assert ''.join(result.split()) == ''.join(expected_output.split())
