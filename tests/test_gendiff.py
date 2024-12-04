f24561dae4ca91004b8b07f838eb0d96e4b52486
Chawku <chawku.work@gmail.com>
2024-12-04 17:57:33 +0300

    update tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -9,13 +9,10 @@ def fixture_path():
     return os.path.join("tests", "fixtures")
 
 
-@pytest.fixture
-def load_file_content(fixture_path):
-    def _load_file(filename):
-        file_path = os.path.join(fixture_path, filename)
-        with open(file_path, 'r') as f:
-            return f.read()
-    return _load_file
+def get_content(fixture_path, filename):
+    file_path = os.path.join(fixture_path, filename)
+    with open(file_path, 'r') as f:
+        return f.read()
 
 
 @pytest.mark.parametrize(
@@ -37,15 +34,11 @@ def load_file_content(fixture_path):
         ),
     ],
 )
-def test_generate_diff(
-    file1, file2, expected_file, format_of_output,
-    load_file_content, fixture_path
-):
+def test_generate_diff(file1, file2, expected_file, format_of_output, fixture_path):
     file1_path = os.path.join(fixture_path, file1)
     file2_path = os.path.join(fixture_path, file2)
-    expected_output = load_file_content(expected_file)
+    expected_output = get_content(fixture_path, expected_file)
 
-    result = generate_diff(file1_path, file2_path,
-                           format_of_output=format_of_output)
+    result = generate_diff(file1_path, file2_path, format_of_output=format_of_output)
 
     assert ''.join(result.split()) == ''.join(expected_output.split())
